51d90bd6449a27b806a42b7fd316bd9cc79f7d65
JavierOramas <javiale2000@gmail.com>
2021-03-10 20:02:40 -0500

    cp 2 menos el 5

--- cp2/isoclinas.py
+++ cp2/isoclinas.py
@@ -1,16 +1,30 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-data = np.linspace(-5,5, 10)
+def graficar_isoclina(func, func_str):
+    
+    data = np.linspace(-15,15, 10)
+    
+    fig, ax = plt.subplots()
+    ax.plot([-15,15],[0,0], color='black')
+    ax.plot([0,0],[-15,15],  color='black')
+    
+    for j in range(-3,3):
+        y = []
+        for i in data:
+            y.append(func(i))
+        ax.plot(data,y, color = 'blue')
+        ax.grid(b=True)
+        ax.axis()
+        ax.set_ylim(-15,15)
+        ax.set_xlim(-15,15)
+        ax.set_title(f"Isoclina de {func_str}")
+    
+    save_name = func_str.split(",")[0]
+    fig.savefig(f'isoclina_{save_name}.png')
+    
+a = lambda x : - x
+graficar_isoclina(a,"y'=x+y, y(0) = 0, k = 0")
 
-func = lambda x, k: -x + k 
-
-for j in range(-3,3):
-    y = []
-    for i in data:
-        y.append(func(i,j))
-    plt.plot(data,y)
-    plt.legend([f'k={item}' for item in range(-3,3)])
-    plt.grid(b=True)
-    plt.axis()
-plt.savefig('isoclinas_a.png')
+b = lambda x : 2 * x - 8 
+graficar_isoclina(b,"y'=2x-y, y(4) = 0, k = 8")
